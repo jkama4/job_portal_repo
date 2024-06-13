606a0c190c6b20015fab5b07249a85414c50001f
gretakud <119207051+gretakud@users.noreply.github.com>
2024-06-13 17:23:17 +0200

    Update urls.py

--- final_app/urls.py
+++ final_app/urls.py
@@ -3,10 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-<<<<<<< HEAD
     path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact")
-=======
+    path("contact/", views.contact, name="contact"),
     path("results/", views.results, name="results"),
->>>>>>> jayden_branch
 ]
