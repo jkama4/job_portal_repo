d05a7cde93d662e1eb3a47d2ac0d30b2a47f2810
gretakud <119207051+gretakud@users.noreply.github.com>
2024-06-13 17:26:18 +0200

    Update views.py

--- final_app/views.py
+++ final_app/views.py
@@ -1,26 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
-# from .api import get_mars_rovers
 
 def home(request):
     return render(request, 'home.html')
 
-<<<<<<< HEAD
 def about(request):
     return render(request, 'about.html')
 
-def contact(request):
-    return render(request, 'contact.html')
-=======
 def results(request):
     query = request.GET.get('q', '')
     context = {'q': query}
     return render(request, 'results.html', context)
 
-### View for job application
-
-
-
-
-
->>>>>>> jayden_branch
+def contact(request):
+    return render(request, 'contact.html')
