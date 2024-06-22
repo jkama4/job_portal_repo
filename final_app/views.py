8ee494c901c07a4c39b2dc4ef8f23ac2e622717e
jkama4 <142435066+jkama4@users.noreply.github.com>
2024-06-22 11:26:17 +0200

    Update views.py

--- final_app/views.py
+++ final_app/views.py
@@ -1,16 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from typing import Dict, List
 from django.http import HttpResponse, HttpRequest
+from .api import google_job_api
+from django.urls import reverse
+from typing import List, Dict, Any
+from .models import Job
+from .forms import JobForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Job
 
-def home(request):
-    return render(request, 'home.html')
+def home(request: HttpRequest) -> HttpResponse:
+    """View for home.html
+    param: request (HttpRequest) : Makes a request to present the html
+    return: renders the data
+    """
+    return render(request, "home.html")
 
-def about(request):
-    return render(request, 'about.html')
+def about(request: HttpRequest) -> HttpResponse:
+    """View for about.html
+    param: request (HttpRequest) : Makes a request to present the html
+    return: renders the data
+    """
+    return render(request, "about.html")
 
-def results(request):
-    query = request.GET.get('q', '')
-    context = {'q': query}
-    return render(request, 'results.html', context)
+def results(request: HttpRequest) -> HttpResponse:
+    """View for results.html
+    param: request (HttpRequest) : Makes a request to present the html
+    return: renders the data
+    """
+    country: str = request.GET.get("country", "")
+    city: str = request.GET.get("city", "")
+    location: str = city if city else country
+    query: str = request.GET.get("q", "")
+    if not query:
+        return redirect(reverse("home"))
 
-def contact(request):
-    return render(request, 'contact.html')
+    try:
+        jobs = google_job_api(query=query, location=location)
+    except KeyError:
+        return redirect('error')
+    
+    context: Dict[str,Any] = {
+        "q": query,
+        "location": location,
+        "jobs": jobs
+    }
+    
+    return render(request, "results.html", context)
+
+def contact(request: HttpRequest) -> HttpResponse:
+    """View for contact.html
+    param: request (HttpRequest) : Makes a request to present the html
+    return: renders the data
+    """
+    return render(request, "contact.html")
+
+def error(request: HttpRequest) -> HttpResponse:
+    """View for error.html
+    param: request (HttpRequest) : Makes a request to present the html
+    return: renders the data
+    """
+    return render(request, "error.html")
+
+def favourites(request: HttpRequest) -> HttpResponse:
+    """View for favourites.html
+    param: request (HttpRequest) : Makes a request to present the html
+    return: renders the data
+    """
+    return render(request, "favourites.html")
