60280788291c85482470bff17f847b549f7cc25a
gretakud <119207051+gretakud@users.noreply.github.com>
2024-06-22 20:39:34 +0200

    Update models.py

--- final_app/models.py
+++ final_app/models.py
@@ -3,11 +3,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Job(models.Model):
-    title = models.CharField(max_length=200)
-    company_name = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
-    link = models.URLField(max_length=500)
-    image = models.URLField(max_length=500, null=True, blank=True)
+    """ A model representing a job listing.
 
-    def __str__(self):
+    attribute: title (str): The title of the job.
+    attribute: company_name (str): The name of the company offering the job.
+    attribute: location (str): The location where the job is based.
+    attribute: link (str): A URL link to the job listing.
+    attribute: image (Optional[str]): A URL link to an image related to the job listing (optional).
+    """
+    title: str = models.CharField(max_length=200)
+    company_name: str = models.CharField(max_length=200)
+    location: str = models.CharField(max_length=200)
+    link: str = models.URLField(max_length=500)
+    image: Optional[str] = models.URLField(max_length=500, null=True, blank=True)
+
+    def __str__(self) -> str:
+        """ Returns the string representation of the Job instance.
+        return: str: The title of the job.
+        """
         return self.title
