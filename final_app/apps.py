e372dd18dd51355bf2f57851ccacea4b2f38332a
gretakud <119207051+gretakud@users.noreply.github.com>
2024-06-22 16:31:07 +0200

    Update apps.py

--- final_app/apps.py
+++ final_app/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class FinalAppConfig(AppConfig):
+    """ Configuration class for the final_app application. """
+    
     default_auto_field = "django.db.models.BigAutoField"
     name = "final_app"
