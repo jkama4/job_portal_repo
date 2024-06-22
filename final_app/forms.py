85f300504d7b7e72940a4ce9baefd43e65889a64
gretakud <119207051+gretakud@users.noreply.github.com>
2024-06-22 16:40:01 +0200

    Update forms.py

--- final_app/forms.py
+++ final_app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Job
 
 class JobForm(forms.ModelForm):
+    """ A form for creating and updating Job instances. """
     class Meta:
         model = Job
         fields = ['title', 'company_name', 'location', 'link', 'image']
