92b892de7087af5cbec67a8e0f8cebec0ee61675
jkama4 <142435066+jkama4@users.noreply.github.com>
2024-06-22 11:27:21 +0200

    Update urls.py

--- final_app/urls.py
+++ final_app/urls.py
@@ -1,9 +1,14 @@
+# myapp/urls.py
 from django.urls import path
 from . import views
+from typing import List, Any
+from django.contrib.auth import views as auth_views
 
-urlpatterns = [
+urlpatterns: List[Any] = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("results/", views.results, name="results"),
+    path("error/", views.error, name="error"),
+    path("favourites/", views.favourites, name="favourites"),
 ]
