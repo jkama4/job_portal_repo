68300e71d4f6c009178e46cdbd58ef8b5496d9d7
jkama4 <142435066+jkama4@users.noreply.github.com>
2024-06-24 17:00:30 +0200

    Update forms.py

--- final_app/forms.py
+++ final_app/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from .models import Job
+from typing import List
 
 class JobForm(forms.ModelForm):
     """ A form for creating and updating Job instances. """
     class Meta:
         model = Job
-        fields = ['title', 'company_name', 'location', 'link', 'image']
+        fields: List[str] = ['title', 'company_name', 'location', 'link', 'image']
